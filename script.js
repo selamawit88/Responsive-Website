851457fa5d5f61f53533097628a550caf6ad5118
selamawit Ayalew Mesfin <slmwt_assefa@yahoo.com>
2019-01-22 10:45:20 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+window.onload = function(){
+     var header = document.getElementById("header");
+	 var menuToggle = document.getElementById("toggle");
+	 
+	 menuToggle.addEventListener("click", function(){
+		 if(header.className === "open") {
+			 header.className = "";
+		 }else{
+			 header.className = "open";
+		 }
+		 
+	 });
+} ;
+	 
